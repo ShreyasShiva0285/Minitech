95ef1fa07b0f1ae3590797c4683c3c788659351f
Shreyas Shiva <shreyasshiva29@gmail.com>
2025-08-05 11:50:26 +0100

    Update app M.py

--- app M.py
+++ app M.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 import numpy as np
+pip install streamlit pandas plotly scikit-learn numpy
+
 
 # -------------------- Styling --------------------
 st.set_page_config(layout="wide")
